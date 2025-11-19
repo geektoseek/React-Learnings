58ce2b2a3c899a42f8f615ebd1c5ea99496aaed8
naeem <naeeminfo120@gmail.com>
2025-11-19 11:36:24 +0500

    just updating 11/19/2025

--- src/Components/Greeting.jsx
+++ src/Components/Greeting.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FaBookDead } from "react-icons/fa";
+
 
 const Salam = "Assalamu Alaikum";
 const date = new Date();
@@ -7,6 +9,7 @@ const Greeting = () => {
     return (
         <>
             <h1>{Salam}</h1>
+            <FaBookDead />
             <p>Current Date : {date.getDate()}</p>
             <h2>Current Month : {date.getMonth()}</h2>
             <h2>Current Year : {date.getFullYear()}</h2>
