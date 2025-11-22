98f5ae4257e0dad88ace086cd4dc76e82d1a6c2c
naeem <naeeminfo120@gmail.com>
2025-11-22 19:34:40 +0500

    Shopping List Done

--- src/Components/ShoppingList.jsx
+++ src/Components/ShoppingList.jsx
@@ -5,6 +5,19 @@ const ShoppingList = () => {
     const [name, setNames] = useState('');
     const [quantity, setQuantity] = useState('');
 
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        if (!name || !quantity) return;
+
+        const newItem = {
+            name,
+            quantity: parseInt(quantity)
+        };
+        setItems((prevItems) => [...prevItems, newItem])
+        setNames("");
+        setQuantity("");
+    }
 
 
     return (
@@ -12,7 +25,18 @@ const ShoppingList = () => {
             <h1>Here is my Shopping List</h1>
             <form onSubmit={handleSubmit}>
                 <input type="text" placeholder='Enter Item Name' value={name} onChange={(e) => setNames(e.target.value)} />
+                <input type="number" placeholder='Enter items Quantity' value={quantity} onChange={(e) => setQuantity(e.target.value)} />
+                <button type='submit'>Add To Cart</button>
             </form>
+
+
+            <ul>
+                {items.map((item, index) => (
+                    <li key={index}>
+                        {item.name} - Quantity :{item.quantity}
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
