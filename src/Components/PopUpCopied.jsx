9b4f0e7d9f4e14c05cdb889075e023a9f1b676d7
naeem <naeeminfo120@gmail.com>
2025-11-23 18:04:08 +0500

    Portal Practice Done by changing the Div in HTML

--- src/Components/PopUpCopied.jsx
+++ src/Components/PopUpCopied.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import { createPortal } from "react-dom";
 
 const PopUpCopied = ({ copied }) => {
-    return (
-        <>
+    return createPortal(
+        <section>
             {copied && (
                 <div className='max-w-max bg-slate-700 rounded-xl text-yellow-200 p-2' >
                     copy to clipboard
                 </div >
             )}
-        </>
+        </section>,
+        document.querySelector("#portal-sec")
     )
-}
+};
 
 export default PopUpCopied
