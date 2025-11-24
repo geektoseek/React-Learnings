a65a96e4cac8601f711721733f076d4f16ace3cd
naeem <naeeminfo120@gmail.com>
2025-11-24 13:33:00 +0500

    Toogle Switcher in React js

--- src/Components/Switcher.jsx
+++ src/Components/Switcher.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { use, useState } from 'react'
 
 const Switcher = () => {
+
+    const [sw, setSw] = useState(false)
+
     return (
         <>
-            <h1>Switcher</h1>
+            {sw ? <span>Dark</span> : <span>Light</span>}
+            <br />
+            <input type="text" key={sw ? "dark" : "light"} />
+            <button onClick={() => setSw((s) => !s)}>Switch</button>
         </>
     )
 }
