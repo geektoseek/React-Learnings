713f5c286d0d6d9a35da989fc2931d5f6de69bed
naeem <naeeminfo120@gmail.com>
2025-11-25 01:17:06 +0500

    Started learning UseEffact

--- src/Components/UseEffact.jsx
+++ src/Components/UseEffact.jsx
@@ -1,24 +1,54 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+// import React from 'react'
+// import { useState, useEffect } from 'react'
+// const UseEffact = () => {
 
+//     const [value, setValue] = useState(0)
+//     useEffect(() => {
+//         console.log("Working On UseEffact");
+//         document.title = `Increament${value}`
 
-const UseEffact = () => {
+//     })
+//     return (
+//         <>
+//             <h2>{value}</h2>
+//             <button onClick={() => setValue(value + 1)}>Click Me</button>
+//         </>
+//     )
+// }
+// export default UseEffact
 
-    const [value, setValue] = useState(0)
+import React, { useEffect } from 'react'
+import { useState } from 'react';
 
+export const UseEffact = () => {
+
+    const [data, setData] = useState([]);
     useEffect(() => {
-        console.log("Working On UseEffact");
-        document.title = `Increament${value}`
 
-    })
+        async function getData() {
+            const response = await fetch(
+                "https://jsonplaceholder.typicode.com/posts"
+            );
+            const data = await response.json();
+            if (data && data.length) setData(data)
 
+        }
+
+        getData();
+    }, []);
 
     return (
-        <>
-            <h2>{value}</h2>
-            <button onClick={() => setValue(value + 1)}>Click Me</button>
-        </>
+        <div>
+            <ul>
+                {data.map(todo => (
+                    <section key={Math.random()}>
+                        <li>{todo.title}</li>
+                        <li>body ----{todo.body}</li>
+                    </section>
+                ))}
+            </ul>
+
+        </div >
     )
 }
-
-export default UseEffact
+export default UseEffact; 
